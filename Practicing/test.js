4183c38fae2fa2e31b77f324113f591894fe5a45
NassimZahri <nassimzahri02@gmail.com>
2023-04-08 17:37:22 +0000

    module 4 solution

--- Practicing/test.js
+++ Practicing/test.js
@@ -1,19 +1,24 @@
 
-// AUTO_REFRESH_STOPPER
+SAFETY_ON
 
-function makeMultiplier( multiplier ){
-    var myFunc = function (x){
-        return multiplier * x;
-    }
-    return myFunc;
-}
+// function makeMultiplier( multiplier ){
+//     var myFunc = function (x){
+//         return multiplier * x;
+//     }
+//     return myFunc;
+// }
 
-var multiplyby3 = makeMultiplier(3);
-console.log(multiplyby3(10));
+// var multiplyby3 = makeMultiplier(3);
+// console.log(multiplyby3(10));
 
-function OP(x, operation){
-    return operation(x);
-}
+// function OP(x, operation){
+//     return operation(x);
+// }
 
-var result = OP(5,multiplyby3)
-console.log(result);
+// var result = OP(5,multiplyby3)
+// console.log(result);
+
+// var x = {a: 5,b: 3};
+// var y = x;
+
+// console.log(y);
